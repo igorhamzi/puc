90f03ad2716147970b67699b5784a417ab13fbdd
Igor Hamzi <igorwhamzi@gmail.com>
2023-11-02 19:16:38 -0300

    add list 04 Strings

---
+++ periodo-1/algoritmos-e-tecnicas-de-programacao/Listas/lista04-strings/Program.cs
@@ -0,0 +1,19 @@
+﻿// 4° lista de exercícios(34) - String
+
+using System;
+
+
+namespace exercicio_01
+{
+    class Ex01
+    {
+        public static void Main(string[]args)
+        {
+            int[] a = {1,0,5,-2,-5,7};
+            int soma = a[0] + a[1] + a[5];
+            Console.WriteLine(soma);
+            a[4] = 100;
+            for (int i = 0; i < 6; i++) Console.WriteLine(a[i]);
+        }
+    }
+}
