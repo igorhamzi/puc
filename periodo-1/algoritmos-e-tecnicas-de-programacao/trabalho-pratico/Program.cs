f6d01057f84833f641eb7c0e043bd4a0926bec15
Igor Hamzi <igorwhamzi@gmail.com>
2023-11-09 22:13:22 -0300

    adds preparation to the game

--- periodo-1/algoritmos-e-tecnicas-de-programacao/trabalho-pratico/Program.cs
+++ periodo-1/algoritmos-e-tecnicas-de-programacao/trabalho-pratico/Program.cs
@@ -72,6 +72,75 @@ class Jogador{
 
 class Bingo{
     public static void Main(){
-        
+        int qntJogadores;
+
+        do{
+            Console.WriteLine("Quantas pessoas irão jogar? (min:2 max:5)");
+            qntJogadores = int.Parse(Console.ReadLine());
+        } while (qntJogadores < 2 || qntJogadores > 5);
+
+        Jogador[] jogadores = new Jogador[qntJogadores];
+
+        for (int i = 0; i < jogadores.Length; i++){
+            Console.WriteLine($"jogador {i + 1}: qual? seu nome");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine($"jogador {i + 1}: qual sua idade?");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"jogador {i + 1}: qual seu sexo?");
+            string sexo = Console.ReadLine();
+
+            int qntCartelas;
+
+            do{
+                Console.WriteLine($"jogador {i + 1}: quantas cartelas você quer? (min:1 max:4)");
+                qntCartelas = int.Parse(Console.ReadLine());
+            } while (qntCartelas < 1 || qntCartelas > 4);
+
+            jogadores[i] = new Jogador(nome, sexo, idade, qntCartelas);
+        }
+        /* IMPRIME AS CARTELAS DOS JOGADORES
+        for (int i = 0; i < x; i++){
+            Console.WriteLine($"Imprimir as cartelas do {jogadores[i].nome}:");
+            Console.Write($"\n");
+
+            
+            int qnt = jogadores[i].qntCartelas;
+
+            switch(qnt){
+                case 1:
+                    Cartela.ImprimirCartela(jogadores[i].cartela1);
+                break;
+                case 2:
+                    Cartela.ImprimirCartela(jogadores[i].cartela1);
+                    Console.WriteLine($"\n");
+                    Cartela.ImprimirCartela(jogadores[i].cartela2);
+                break;
+                case 3:
+                    Cartela.ImprimirCartela(jogadores[i].cartela1);
+                    Console.WriteLine($"\n");
+
+                    Cartela.ImprimirCartela(jogadores[i].cartela2);
+                    Console.WriteLine($"\n");
+                    Cartela.ImprimirCartela(jogadores[i].cartela3);
+                break;
+                case 4:
+                    Cartela.ImprimirCartela(jogadores[i].cartela1);
+                    Console.WriteLine($"\n");
+                    Cartela.ImprimirCartela(jogadores[i].cartela2);
+                    Console.WriteLine($"\n");
+                    Cartela.ImprimirCartela(jogadores[i].cartela3);
+                    Console.WriteLine($"\n");
+                    Cartela.ImprimirCartela(jogadores[i].cartela4);
+                break;
+            }
+            Console.WriteLine($"\n");
+            Console.WriteLine($"\n");
+            Console.WriteLine($"\n");
+        }
+        */
+
+        Console.WriteLine("Todos com suas devidas cartelas, vamos iniciar o BINGO!");
     }
 }
