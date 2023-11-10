d6d85137584851443bf54f1f40de9a0216fec0b8
Igor Hamzi <igorwhamzi@gmail.com>
2023-11-09 22:10:28 -0300

    feat: add class Cartela

--- periodo-1/algoritmos-e-tecnicas-de-programacao/trabalho-pratico/Program.cs
+++ periodo-1/algoritmos-e-tecnicas-de-programacao/trabalho-pratico/Program.cs
@@ -2,6 +2,32 @@
 
 using System;
 
+class Cartela{
+/*
+    private int qntCartelas;
+*/
+    public static int[,] ObterCartela(){
+        Random rnd = new Random();
+        int [,] cartela = new int[5,5];
+        for (int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                if(i == 2 && j == 2) cartela[i,j] = 0;
+                else cartela[i,j] = rnd.Next(1, 75);
+            }
+        }
+        return cartela;
+    }
+
+    public static void ImprimirCartela(int[,] cartela){
+        for (int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                Console.Write($"{cartela[i,j]}  ");
+            }
+            Console.WriteLine($"\n");
+        }
+    }
+}
+
 class Bingo{
     public static void Main(){
         
