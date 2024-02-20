7d497244d15850f86b77cd1d2001dfacb45a8be8
igorhamzi <igorwhamzi@gmail.com>
2024-02-20 11:59:48 -0300

    feat: add ex04 - ends

--- periodo-2/algoritmos-e-estrutura-de-dados/listas/lista01-nivelamento/Lista01.cs
+++ periodo-2/algoritmos-e-estrutura-de-dados/listas/lista01-nivelamento/Lista01.cs
@@ -1,7 +1,7 @@
 ﻿// 1° lista de exercícios(4) - Nivelamento
 
 using System;
-
+/*
 01 - Criar um programa que carregue uma matriz 12 x 4 com os valores (em reais) das vendas de uma loja de
 carros, em que cada linha represente um mês do ano, e cada coluna, uma semana do mês. Para fins de
 simplificação considere que cada mês possui somente 4 semanas. Calcule e imprima:
@@ -112,3 +112,55 @@ namespace exercicio_03
         }
     }
 }
+
+
+04 - Leia dois vetores de inteiros x e y, cada um com 5 elementos (assuma que o usuário não informará elementos
+repetidos). Calcule e mostre o resultado das seguintes operações:
+• Soma entre x e y: soma de cada elemento de x com o elemento da mesma posição em y.
+• Produto entre x e y: multiplicação de cada elemento de x com o elemento da mesma posição em y.
+• Diferença entre x e y: todos os elementos de x que não existam em y.
+• Interseção entre x e y: apenas os elementos que aparecem nos dois vetores.
+• Uniao entre x e y: todos os elementos de x, e todos os elementos de y que não estão em x
+
+namespace exercicio_04
+{
+    class Ex04
+    {
+        public static void Main(string[] args)
+        {
+            int [] x = new int [5];
+            int [] y = new int [5];
+
+            for (int i= 0; i < 5; i++){
+                Console.WriteLine($"Informe o {i+1}° valor do 1° vetor");
+                x[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Informe o {i+1}° valor do 2° vetor");
+                y[i] = int.Parse(Console.ReadLine());
+            }
+
+            int [] sum = new int [5];
+            int [] multiplication = new int [5];
+            int [] difference = new int [5];
+            int [] intersection = new int [10];
+            int countDifference = 0;
+            int countIntersection = 0;
+
+            for (int i= 0; i < 5; i++){
+                sum[i] = x[i] + y[i];
+                multiplication[i] = x[i] * y[i];
+                bool equals = false;
+                for(int j = 0; j < 5; j++){
+                    if(x[i] == y[j]){
+                        equals = true;
+                        intersection[countIntersection] = x[i];
+                    }
+                }
+                if (!equals){
+                    difference[countDifference] = x[i];
+                    countDifference++;
+                } 
+            } 
+        }
+    }
+}
+*/
